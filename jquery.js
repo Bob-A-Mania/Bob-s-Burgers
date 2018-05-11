8a50b34b7affff448311e0c4718fc75d43cc21bf
Bob Richards <37851526+Bob-A-Mania@users.noreply.github.com>
2018-05-11 11:18:21 -0600

    Add files via upload

---
+++ jquery.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+
+  $("li").mouseover(function(){
+    $(this).css("color" , "#e6eaef");
+  });
+
+  $("li").mouseout(function(){
+    $(this).css("color" , "#b0b3b7");
+  });
+
+});
